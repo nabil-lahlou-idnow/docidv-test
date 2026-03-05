d1e8849a4eee7160fac47af20acfd8137dd93611
docidv-developer <docidv-developer@idnow.io>
2026-03-05 10:50:31 +0100

    Release - Update SDK to version 1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 # Changelog
 
+## [1.0.2] - 2026-03-05
+- Fixe Update Package.swift
+- Update ReadME
+
 ## [1.0.1] - 2026-03-05
 Fixe Update Package.swift
--- Package.swift
+++ Package.swift
@@ -23,23 +23,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DocIDVCommon",
-            url: "https://github.com/nabil-lahlou-idnow/docidv-test/releases/download/1.0.1/DocIDVCommon.xcframework.zip",
-            checksum: "6e65f4a60ce63de3f718dd684fe7a65563c17098b4ece69e4d9131e491b8e6af"
+            url: "https://github.com/nabil-lahlou-idnow/docidv-test/releases/download/1.0.2/DocIDVCommon.xcframework.zip",
+            checksum: "588bf60c03ddb67f31ca76187d74550b668c90814b4092cf7dc3c8d1d0d44e97"
         ),
         .binaryTarget(
             name: "DocIDV",
-            url: "https://github.com/nabil-lahlou-idnow/docidv-test/releases/download/1.0.1/DocIDV.xcframework.zip",
-            checksum: "196ff6f7a74ff1d7bce98c9f44457fca32eff7c49e23374835734809a725ac05"
+            url: "https://github.com/nabil-lahlou-idnow/docidv-test/releases/download/1.0.2/DocIDV.xcframework.zip",
+            checksum: "dcbfed7a301bf795223f0293d98fa050ca14c3f01fa8e912137bc29807aca613"
         ),
         .binaryTarget(
             name: "DocIDVAIModule",
-            url: "https://github.com/nabil-lahlou-idnow/docidv-test/releases/download/1.0.1/DocIDVAIModule.xcframework.zip",
-            checksum: "c47856899c7884fa5980d197cfd8246727eea960794a8e877f62d49f25dad843"
+            url: "https://github.com/nabil-lahlou-idnow/docidv-test/releases/download/1.0.2/DocIDVAIModule.xcframework.zip",
+            checksum: "3e6d1fe4bf0a6fad1d828f3ceb470d5ac723747e0548cf7508381084f5250c6c"
         ),
         .binaryTarget(
             name: "DocIDVEIDGovernikusModule",
-            url: "https://github.com/nabil-lahlou-idnow/docidv-test/releases/download/1.0.1/DocIDVEIDGovernikusModule.xcframework.zip",
-            checksum: "2912e6d71717e3ea2c16425121d482832e747e2a66aa7202fe9eaabcca8a06f3"
+            url: "https://github.com/nabil-lahlou-idnow/docidv-test/releases/download/1.0.2/DocIDVEIDGovernikusModule.xcframework.zip",
+            checksum: "9e627322cb13830099aee14fb2b67cf7692d62dc40da58d8f8d7ab980c7460fa"
         ),
         .target(
             name: "DocIDV-Target-Wrapper",
@@ -59,7 +59,7 @@ let package = Package(
                 "DocIDVAIModule",
                 "DocIDVEIDGovernikusModule",
                 .product(name: "DocIDVAI", package: "docidv-ai-test"),
-                .product(name: "EIDGovernikus", package: "eid-deploy-test")
+                .product(name: "IDnowEIDGovernikus", package: "eid-deploy-test")
             ],
             path: "sources-eid-governikus"
         )
